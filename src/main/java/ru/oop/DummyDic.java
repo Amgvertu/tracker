1a74e919afc5b0353c04e1301825e1326b690884
amgvertu <amgvertu@gmail.com>
2025-04-29 09:33:14 +0700

    7.5 Вызов метода с возвращаемым типом

---
+++ src/main/java/ru/oop/DummyDic.java
@@ -0,0 +1,14 @@
+package ru.oop;
+
+public class DummyDic {
+    public String engToRus(String eng) {
+        return "Неизвестное слово. " + eng;
+    }
+
+    public static void main(String[] args) {
+        DummyDic dic = new DummyDic();
+        String answer = dic.engToRus("Dictionary");
+        System.out.println(answer);
+
+    }
+}
