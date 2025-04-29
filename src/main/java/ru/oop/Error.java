5666ef345ca511022a8b731b959c1c13517915a1
amgvertu <amgvertu@gmail.com>
2025-04-29 23:50:12 +0700

    7.8 Конструктор

---
+++ src/main/java/ru/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Актуальная ошибка: " + active);
+        System.out.println("Номер: " + status);
+        System.out.println("Описание: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error emptyError = new Error();
+        Error notFound = new Error(true, 404, "Address not found");
+        Error badRequest = new Error(true, 400, "Request is not valid");
+        emptyError.printInfo();
+        notFound.printInfo();
+        badRequest.printInfo();
+    }
+
+}
