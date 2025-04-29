0c14a9f42306e65cb10db4b4118a0de415cbc7eb
amgvertu <amgvertu@gmail.com>
2025-04-29 13:48:15 +0700

    7.7 Взаимодействие объектов

--- src/main/java/ru/oop/Ball.java
+++ src/main/java/ru/oop/Ball.java
@@ -3,7 +3,10 @@ package ru.oop;
 public class Ball {
 
     public void tryRun(boolean condition) {
-        if (condition) System.out.println("Колобок съеден");
-        else System.out.println("Колобок сбежал");
+        if (condition) {
+            System.out.println("Колобок съеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
     }
 }
