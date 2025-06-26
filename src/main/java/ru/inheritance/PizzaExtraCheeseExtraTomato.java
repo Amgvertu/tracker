60f633d569ce7aef653cc69d113937e366f17507
amgvertu <amgvertu@gmail.com>
2025-06-26 09:33:42 +0700

    8.05 Вызов переопределенного метода родителя super method

---
+++ src/main/java/ru/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,9 @@
+package ru.inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
+
+    @Override
+    public String name() {
+        return super.name() + " + extra tomato";
+    }
+}
