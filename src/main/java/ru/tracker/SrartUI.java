47ae989966232205c35c44f3bdf12ab09c3130ba
amgvertu <amgvertu@gmail.com>
2025-08-13 09:49:27 +0700

    9.12 toString()

--- src/main/java/ru/tracker/SrartUI.java
+++ src/main/java/ru/tracker/SrartUI.java
@@ -8,5 +8,6 @@ public class SrartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String itemFormatting = item.getCreated().format(formatter);
         System.out.println(itemFormatting);
+        System.out.println(item);
     }
 }
