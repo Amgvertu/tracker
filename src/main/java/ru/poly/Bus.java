c930a18c82d04d25e2b3cb44e865e16e938d0fc7
amgvertu <amgvertu@gmail.com>
2025-09-11 08:27:55 +0700

    10.13 Input

---
+++ src/main/java/ru/poly/Bus.java
@@ -0,0 +1,21 @@
+package ru.poly;
+
+public class Bus implements Transport {
+
+    double price;
+
+    @Override
+    public void drive() {
+        System.out.println("Автобус едет...");
+    }
+
+    @Override
+    public void passenger(int passengers) {
+        System.out.println("В автобусе " + passengers + "пассажиров...");
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return this.price * fuel;
+    }
+}
---
+++ src/main/java/ru/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.poly;
+
+public interface Transport {
+    void drive();
+
+    void passenger(int passenger);
+
+    double refuel(double fuel);
+}
