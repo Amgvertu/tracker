d42b3937badbd05036e71811736978796bb4843a
amgvertu <amgvertu@gmail.com>
2025-09-13 17:21:22 +0700

    10.24 Тесты на StartUI

--- src/test/java/ru/tracker/StartUITest.java
+++ src/test/java/ru/tracker/StartUITest.java
@@ -1,4 +1,51 @@
 package ru.tracker;
-class StartUITest {
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class StartUITest {
+    @Test
+    void whenCreateItem() {
+        Input input = new MockInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
+    }
+
+    @Test
+    void whenReplaceItem() {
+        Input input = new MockInput(
+                new String[] {"0", "Item name", "1", "1", "Replaced Item", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Replaced Item");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Input input = new MockInput(
+                new String[] {"0", "Item name", "1", "1", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(1)).isNull();
+    }
 }
+
