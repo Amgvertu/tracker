f45c8f519e818655af2b38160e399851103c1c3b
amgvertu <amgvertu@gmail.com>
2025-09-22 19:02:29 +0700

    11.05 Пользовательские исключения

--- src/main/java/ru/ex/FindEl.java
+++ src/main/java/ru/ex/FindEl.java
@@ -2,13 +2,12 @@ package ru.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int result = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
-                result = i;
+            if (key.equals(value[i])) {
+                return i;
             }
         }
-        return result;
+        throw new ElementNotFoundException("Элемент не найден");
     }
 
     public static void main(String[] args) {
