71feda637ecf9c7c3ca7b5a53642d6d2dbb53fc3
amgvertu <amgvertu@gmail.com>
2025-09-24 19:57:15 +0700

    11.08 Обеспечить бесперебойную работу приложения Tracker

--- src/main/java/ru/tracker/input/ValidateInput.java
+++ src/main/java/ru/tracker/input/ValidateInput.java
@@ -3,10 +3,12 @@ package ru.tracker.input;
 public class ValidateInput extends ConsoleInput {
     @Override
     public int askInt(String question) {
+        boolean isValid = false;
         int value = -1;
-        while (value == -1) {
+        while (!isValid) {
             try {
                 value = super.askInt(question);
+                isValid = true;
             } catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите корректные данные");
             }
